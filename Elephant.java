f10d260d99a984f345934c646d175aa80da1b3a1
jamie wang <bombyjamie@gmail.com>
2024-09-23 13:25:30 -0400

    used sprites and animated the elephant

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    public Elephant() {
+        
+        for(int i = 0; i < idle.length; i++) {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
     public void act()
     {
        if(Greenfoot.isKeyDown("left")) {
@@ -19,6 +27,13 @@ public class Elephant extends Actor
        }
        
        eat();
+       animateElephant();
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant() {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
     }
     
     public void eat() {
